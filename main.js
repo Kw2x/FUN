aca2893d946516b2c842c3f7f3556f5073495a89
kw2x <bhen2014@gmail.com>
2022-10-20 22:37:48 -0400

    added borders

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ box1.className = "box1";
 // add test to the box1 element
 box1.textContent = "i am box 1";
 // adds styling to the box1 element
-box1.style = "color: red; font-size: 25px; font-weight: bold"
+box1.style = "color: red; font-size: 25px; font-weight: bold; border: thick groove red; border-radius:5px;"
 // creates a clcik event for the box1 element
 box1.addEventListener("click",b1)
 
@@ -32,33 +32,6 @@ document.body.append(box1);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /* This is an empty div element that will be used to display the box3 
 element under the box1 element*/
 const wrapper = document.createElement("wrapper");
@@ -74,7 +47,7 @@ function bx2() {
 //creates the the box3 div element
 const box3 = document.createElement("box3");
 box3.textContent = "i am box 3";
-box3.style = "color: yellow; font-size: 35px; font-weight: bold"
+box3.style = "color: yellow; font-size: 35px; font-weight: bold; border: thick outset yellow; border-radius:5px;"
 box3.addEventListener("click", b3)
 function b3() {
     alert("box3  has been clicked");
